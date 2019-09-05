8bb6c4e201f4e830bc207c7847facdff07357d5a
Fabian Kramer <fabian.kramer@ionesoft.ch>
2019-09-05 14:38:23 +0200

    subfolder + data projects 6,7,8

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// toggle cards together with clicked button to collapse element
+const cards = document.querySelectorAll('.card');
+
+
+
+// loop through cards, attach event listener to button details and toggle class of card parent
+for (let i = 0; i < cards.length; i++) {
+    let thisCard = cards[i];
+    let parentDiv = thisCard.parentNode;
+    let thisCollapse = thisCard.querySelector('.collapse');
+    let detailsButton = thisCard.querySelector('.details');
+    detailsButton.addEventListener('click', () => {
+
+      parentDiv.classList.toggle('col-md-6');
+      parentDiv.classList.toggle('col-lg-4');
+
+      // if (thisCollapse.classList.contains !== "show") {
+      //
+      //   } else if (thisCollapse.classList.contains === "show") {
+      //         parentDiv.classList.toggle('col-md-6', true);
+      //         console.log(parentDiv);
+      //         // parentDiv.classList.add('col-lg-4');
+      //     }
+    });
+
+
+}
