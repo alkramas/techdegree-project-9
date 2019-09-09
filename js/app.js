cf60b835d72fb78fb428dc02312643041f99c3e7
Fabian Kramer <kramas@gmx.ch>
2019-09-09 07:21:38 +0200

    basic filter and reset function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // toggle cards together with clicked button to collapse element
 const cards = document.querySelectorAll('.card');
-
+const filterContainer = document.querySelector('#filter');
 
 
 // loop through cards, attach event listener to button details and toggle class of card parent
@@ -18,3 +18,77 @@ const cards = document.querySelectorAll('.card');
 //
 //     });
 // }
+
+
+
+// filter projects by Skills
+//
+// save filter settings to localStorage
+// 1st function to save value of input element
+function saveInputLocal(inputElementId, storageElementName) {
+  let  selector = "#" + inputElementId;
+  let inputElement = document.querySelector(selector);
+  let inputId = inputElement.id;
+
+  let storageName = inputId;
+  let storageValue = inputElement.value;
+  let storageChecked = inputElement.checked;
+  // console.log('storageName: ' + storageName + ', storageValue is: ' + storageChecked);
+  // console.log(inputElement);
+
+  if (inputElement.type === 'select-one') {
+    // console.log('select element has been changed');
+    localStorage.setItem( storageName, storageValue );
+    // console.log( localStorage.getItem(storageName) );
+  } else if (inputElement.type === 'checkbox') {
+    localStorage.setItem( storageName, storageChecked);
+    // console.log( localStorage.getItem(storageName) );
+  }
+
+};
+// call save function on event change of input
+filterContainer.addEventListener('change', function(e){
+  let id = e.target.id;
+  console.log('the event target object is the element with id #' + id);
+  saveInputLocal(id, id);
+});
+
+// get values in storage
+function getStoredInputValues() {
+  // loop through input elements
+  let inputElements = document.querySelectorAll('.form-check-input');
+  for (i = 0; i < inputElements.length; i += 1) {
+      let thisElement = inputElements[i];
+      let thisId = thisElement.id;
+      // console.log(thisElement);
+      // console.log(thisId);
+
+      let thisStorageValue = localStorage.getItem(thisId);
+      // console.log('the storage value of this element is: ' + thisStorageValue);
+
+      if (thisElement.type === 'select-one') {
+        thisElement.value = thisStorageValue;
+      } else if (thisElement.type === 'checkbox') {
+        if (thisStorageValue === 'false') {
+          thisElement.checked = false;
+        } else if (thisStorageValue === 'true') {
+          thisElement.checked = true;
+        }
+        // thisElement.value = thisStorageValue;
+      }
+  }
+};
+
+getStoredInputValues();
+
+// reset filters
+// call save function on event change of input
+filterContainer.addEventListener('change', function(e){
+  let checkoxes = filterContainer.querySelectorAll('input');
+  for (i = 0; i < checkoxes.length; i += 1) {
+    this.checked = false;
+  }
+  // let id = e.target.id;
+  // console.log('the event target object is the element with id #' + id);
+  // saveInputLocal(id, id);
+});
